8e589e08dd9b40170574cb67058ceea9888be86d
Sopheaktraleng <sopheaktraleng@Sopheaktras-MacBook-Pro.local>
2024-08-09 00:29:15 +0700

    typeorm

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get<string>('DB_TYPE'),
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<string>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [__dirname + './../**/**.entity{.ts,.js}'],
+          synchronize: configService.get<string>('DB_SYNC'),
+          logging: true,
+        } as TypeOrmModuleAsyncOptions;
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
